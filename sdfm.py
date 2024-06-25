f1456c4ca938fde604439ab74395043067c0e50d
nautrw <nautrw@proton.me>
2024-06-25 15:27:35 -0400

    feat: create sdfm.py and add init command

---
+++ sdfm.py
@@ -0,0 +1,38 @@
+import json
+import os
+import typing
+
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer()
+
+
+@app.command()
+def init():
+    """
+    Initialize sdfmpy
+    
+    
+    """
+    
+    if not os.path.isfile('.sdfmpy.json'):
+        with open('.sdfmpy.json', 'w') as file:
+            file.write('[]')
+    
+    with open(".sdfmpy.json", "r") as file:
+        file_content = file.read()
+
+        if file_content != '[]':
+            overwrite = input("`.sdfmpy.json` is already populated or empty. Overwrite? [Y/n]: ")
+        
+    with open('.sdfmpy.json', 'w') as file:
+        if overwrite.lower() != 'y' or overwrite.lower != '':
+            file.write('[]')
+        else:
+            print('Keeping file contents. Cannot proceed.')
+            exit()
+
+    print(f"Initialized sdfmpy in {path}")
+
+app()
