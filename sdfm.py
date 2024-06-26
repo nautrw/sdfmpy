4e90d9d945d721ce88f74d899e28db58448b269d
nautrw <nautrw@proton.me>
2024-06-25 20:14:07 -0400

    feat: add `add` command

--- sdfm.py
+++ sdfm.py
@@ -1,6 +1,8 @@
 import json
 import os
+import shutil
 import typing
+from pathlib import Path
 
 import typer
 from typing_extensions import Annotated
@@ -21,26 +23,44 @@ def init():
     with open(".sdfmpy.json", "r") as file:
         file_content = file.read()
 
-        if file_content != "[]":
-            overwrite = input(
-                "`.sdfmpy.json` is already populated or empty. Overwrite? [Y/n]: "
-            )
+    if file_content != "[]":
+        overwrite = input(
+            "`.sdfmpy.json` is either already populated or empty. Overwrite? [Y/n]: "
+        )
 
     with open(".sdfmpy.json", "w") as file:
         if overwrite.lower() != "y" or overwrite.lower != "":
             file.write("[]")
         else:
-            print("Keeping file contents. Cannot proceed.")
-            exit()
+            exit("Keeping file contents. Cannot proceed.")
 
     print("Initialized sdfmpy in directory")
 
 
 @app.command()
-def add(
-    file: Annotated[str, typer.Argument(help="The file to add and track with sdfmpy")]
-):
-    pass
+def add(path: Annotated[str, typer.Argument(help="The path to add")]):
+    """Adds a path to sdfmpy"""
+
+    if not os.path.exists(path):
+        exit("Invalid path. Cannot proceed.")
+
+    with open(".sdfmpy.json", "r") as file:
+        data = json.load(file)
+        data.append(path)
+
+    with open(".sdfmpy.json", "w") as file:
+        json.dump(data, file, indent=2)
+
+    path_obj = Path(path)
+    target_dir = Path.cwd() / path_obj.name
+
+    if path_obj.is_dir():
+        shutil.copytree(path, target_dir)
+    else:
+        target_dir.mkdir(parents=True, exist_ok=True)
+        shutil.copy(path, target_dir)
+
+    print(f"Added {path} to sdfmpy and cloned it to {target_dir}")
 
 
 app()
